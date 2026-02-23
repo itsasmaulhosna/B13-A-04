aa9fabf0cabced19c470333ad193c3bc7da8823c
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-23 21:18:45 +0600

    Creat a readme file and ans 1st question

---
+++ README.md
@@ -0,0 +1,4 @@
+1. getElementById,it selects an element by using it's id.
+   getElementByClassName,it selects an element by it's class name.
+   querySelector,it uses CSS and return only first matching element,
+   querySelectorAll,it also uses CSS but it returns all matching element.
--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+// get btn
 
 // get id for count
 const totalCount = document.getElementById('total');
@@ -23,9 +24,13 @@ interviewFilteredBtn.addEventListener('click', function () {
 rejectedFilteredBtn.addEventListener('click', function () {
   noJob.classList.remove('hidden');
 });
+// interviewBtn.addEventListener('click', function () {
+//   noJob.classList.add('hidden');
+// });
 
 mainContainer.addEventListener('click', function (event) {
   if (event.target.classList.contains('interview-btn')) {
+    noJob.classList.add('hidden');
     const parenNode = event.target.parentNode.parentNode;
     const mobileApp = parenNode.querySelector('.mobile').innerText;
     const react = parenNode.querySelector('.react').innerText;
@@ -40,66 +45,15 @@ mainContainer.addEventListener('click', function (event) {
       status,
       note,
     };
-    const exist = interviewList.find((item) => item.mobile == cardInfo.mobile);
+    const exist = interviewList.find(
+      (item) => item.mobileApp == cardInfo.mobileApp,
+    );
 
     if (!exist) {
       interviewList.push(cardInfo);
-      // exist.status = 'Interview';
     }
     calculateCount();
-
     renderInterviewing();
-  }
-});
-
-// rejected
-
-mainContainer.addEventListener('click', function (event) {
-  if (event.target.classList.contains('rejected-btn')) {
-    const parenNode = event.target.parentNode.parentNode;
-    const mobileApp = parenNode.querySelector('.mobile').innerText;
-    const react = parenNode.querySelector('.react').innerText;
-    const type = parenNode.querySelector('.type').innerText;
-    const status = parenNode.querySelector('.status').innerText;
-    const note = parenNode.querySelector('.notes').innerText;
-    parenNode.querySelector('.status').innerText = 'Rejectd';
-    const cardInfo = {
-      mobileApp,
-      react,
-      type,
-      status,
-      note,
-    };
-    const exist = interviewList.find((item) => item.mobile == cardInfo.mobile);
-
-    if (!exist) {
-      rejectedList.push(cardInfo);
-    }
-    calculateCount();
-    renderReject();
-    //update jobs
-    // const doneCount = interviewList.length + rejectedList.length;
-    // document.querySelector('.mb-4 p.text-gray-500').innerText =
-    //   `${doneCount} of ${allJobs} jobs`;
-  }
-  // function updateJobsText() {
-  //   const doneCount = interviewList.length + rejectedList.length;
-  //   document.querySelector('.mb-4 p.text-gray-500').innerText =
-  //     `${doneCount} of ${totaljobs} jobs`;
-  // }
-
-  // dlt
-  if (event.target.closest('.fa-trash-can')) {
-    const card = event.target.closest('.card');
-
-    card.remove();
-
-    const totalElement = document.getElementById('total');
-    let total = parseInt(totalElement.innerText);
-
-    if (total > 0) {
-      totalElement.innerText = total - 1;
-    }
   }
 });
 
@@ -107,7 +61,7 @@ mainContainer.addEventListener('click', function (event) {
 function renderInterviewing() {
   filteredSection.innerHTML = '';
   for (let interview of interviewList) {
-    console.log(interview);
+    // console.log(interview);
     let div = document.createElement('div');
     div.innerHTML = `<div
           class="card flex justify-between bg-gray-50 rounded p-4 md:flex-row md:justify-between"
@@ -152,6 +106,99 @@ function renderInterviewing() {
     filteredSection.appendChild(div);
   }
 }
+
+// rejected
+
+mainContainer.addEventListener('click', function (event) {
+  if (event.target.classList.contains('rejected-btn')) {
+    const parenNode = event.target.parentNode.parentNode;
+    const mobileApp = parenNode.querySelector('.mobile').innerText;
+    const react = parenNode.querySelector('.react').innerText;
+    const type = parenNode.querySelector('.type').innerText;
+    const status = parenNode.querySelector('.status').innerText;
+    const note = parenNode.querySelector('.notes').innerText;
+    parenNode.querySelector('.status').innerText = 'Rejectd';
+    const cardInfo = {
+      mobileApp,
+      react,
+      type,
+      status,
+      note,
+    };
+
+    const exist = interviewList.find((item) => item.mobile == cardInfo.mobile);
+
+    if (!exist) {
+      rejectedList.push(cardInfo);
+    }
+    calculateCount();
+    renderReject();
+  }
+
+  // rejected render
+  function renderReject() {
+    filteredSection.innerHTML = '';
+    for (let rejecte of rejectedList) {
+      // console.log(rejecte);
+      let div = document.createElement('div');
+      div.innerHTML = `<div
+          class="card flex justify-between bg-gray-50 rounded p-4 md:flex-row md:justify-between"
+        >
+          <!-- 1 -->
+          <div class="space-y-6">
+            <div>
+              <h3 class="text-blue-950 font-bold mobile">Mobile Frist Crop</h3>
+              <p class="text-gray-400 react">React Native Developer</p>
+            </div>
+            <div class="flex gap-3 flex-col sm:flex-row sm:gap-3 type">
+              <p class="text-gray-400">Remote .</p>
+              <p class="text-gray-400">Full Time .</p>
+              <p class="text-gray-400">$130,000-$175,000</p>
+            </div>
+
+            <p class="px-3 py-3 bg-blue-100 inline-block status">NOT APPLIED</p>
+            <p class="text-gray-500 notes">
+              Bulid cross-platfrom mobile application using React Native.Work on
+              products used by millions of user worldwide.
+            </p>
+
+            <button
+              class="text-green-300 border border-green-200 rounded px-4 py-2"
+            >
+              INTERVIEW
+            </button>
+            <button
+              class="text-red-300 border border-red-100 rounded px-4 py-2"
+            >
+              REJECTED
+            </button>
+          </div>
+          <!-- 2 -->
+
+          <div
+            class="inline-flex rounded-full border border-gray-300 w-fit h-fit p-2"
+          >
+            <i class="fa-regular fa-trash-can"></i>
+          </div>
+        </div>`;
+      filteredSection.appendChild(div);
+    }
+  }
+
+  // dlt
+  if (event.target.closest('.fa-trash-can')) {
+    const card = event.target.closest('.card');
+
+    card.remove();
+
+    const totalElement = document.getElementById('total');
+    let total = parseInt(totalElement.innerText);
+
+    if (total > 0) {
+      totalElement.innerText = total - 1;
+    }
+  }
+});
 
 // rejected render
 function renderReject() {
