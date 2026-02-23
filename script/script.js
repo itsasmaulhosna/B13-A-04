bb54dab8b36d26ceddecb5f8dd4028492e23f5f9
itsasmaulhosna <itsasmaulhosna@gmail.com>
2026-02-23 19:36:00 +0600

    Initialized no jobs section to interview & rejected filtered btn

--- script/script.js
+++ script/script.js
@@ -7,16 +7,21 @@ const interviewCount = document.getElementById('interview');
 const rejectedCount = document.getElementById('rejected');
 // get all cards
 const allCards = document.getElementById('allCard');
-// main
 const mainContainer = document.querySelector('main');
-// filtered
 const filteredSection = document.getElementById('filtered-section');
+const noJob = document.getElementById('no-job');
+
 // get filtered btn
 const allFilteredBtn = document.getElementById('all-filter-btn');
 const interviewFilteredBtn = document.getElementById('interview-filter-btn');
 const rejectedFilteredBtn = document.getElementById('rejected-filter-btn');
+
+// no jobs
+interviewFilteredBtn.addEventListener('click', function () {
+  noJob.classList.remove('hidden');
+});
 rejectedFilteredBtn.addEventListener('click', function () {
-  window.location.assign('/B13-A-04/rejected.html');
+  noJob.classList.remove('hidden');
 });
 
 mainContainer.addEventListener('click', function (event) {
@@ -39,15 +44,119 @@ mainContainer.addEventListener('click', function (event) {
 
     if (!exist) {
       interviewList.push(cardInfo);
-      exist.status = 'Interview';
+      // exist.status = 'Interview';
     }
     calculateCount();
+
     renderInterviewing();
   }
 });
+
+// rejected
+
+mainContainer.addEventListener('click', function (event) {
+  if (event.target.classList.contains('rejected-btn')) {
+    const parenNode = event.target.parentNode.parentNode;
+    const mobileApp = parenNode.querySelector('.mobile').innerText;
+    const react = parenNode.querySelector('.react').innerText;
+    const type = parenNode.querySelector('.type').innerText;
+    const status = parenNode.querySelector('.status').innerText;
+    const note = parenNode.querySelector('.notes').innerText;
+    parenNode.querySelector('.status').innerText = 'Rejectd';
+    const cardInfo = {
+      mobileApp,
+      react,
+      type,
+      status,
+      note,
+    };
+    const exist = interviewList.find((item) => item.mobile == cardInfo.mobile);
+
+    if (!exist) {
+      rejectedList.push(cardInfo);
+    }
+    calculateCount();
+    renderReject();
+    //update jobs
+    // const doneCount = interviewList.length + rejectedList.length;
+    // document.querySelector('.mb-4 p.text-gray-500').innerText =
+    //   `${doneCount} of ${allJobs} jobs`;
+  }
+  // function updateJobsText() {
+  //   const doneCount = interviewList.length + rejectedList.length;
+  //   document.querySelector('.mb-4 p.text-gray-500').innerText =
+  //     `${doneCount} of ${totaljobs} jobs`;
+  // }
+
+  // dlt
+  if (event.target.closest('.fa-trash-can')) {
+    const card = event.target.closest('.card');
+
+    card.remove();
+
+    const totalElement = document.getElementById('total');
+    let total = parseInt(totalElement.innerText);
+
+    if (total > 0) {
+      totalElement.innerText = total - 1;
+    }
+  }
+});
+
+// render
 function renderInterviewing() {
   filteredSection.innerHTML = '';
   for (let interview of interviewList) {
+    console.log(interview);
+    let div = document.createElement('div');
+    div.innerHTML = `<div
+          class="card flex justify-between bg-gray-50 rounded p-4 md:flex-row md:justify-between"
+        >
+          <!-- 1 -->
+          <div class="space-y-6">
+            <div>
+              <h3 class="text-blue-950 font-bold mobile">Mobile Frist Crop</h3>
+              <p class="text-gray-400 react">React Native Developer</p>
+            </div>
+            <div class="flex gap-3 flex-col sm:flex-row sm:gap-3 type">
+              <p class="text-gray-400">Remote .</p>
+              <p class="text-gray-400">Full Time .</p>
+              <p class="text-gray-400">$130,000-$175,000</p>
+            </div>
+
+            <p class="px-3 py-3 bg-blue-100 inline-block status">NOT APPLIED</p>
+            <p class="text-gray-500 notes">
+              Bulid cross-platfrom mobile application using React Native.Work on
+              products used by millions of user worldwide.
+            </p>
+
+            <button
+              class="text-green-300 border border-green-200 rounded px-4 py-2"
+            >
+              INTERVIEW
+            </button>
+            <button
+              class="text-red-300 border border-red-100 rounded px-4 py-2"
+            >
+              REJECTED
+            </button>
+          </div>
+          <!-- 2 -->
+
+          <div
+            class="inline-flex rounded-full border border-gray-300 w-fit h-fit p-2"
+          >
+            <i class="fa-regular fa-trash-can"></i>
+          </div>
+        </div>`;
+    filteredSection.appendChild(div);
+  }
+}
+
+// rejected render
+function renderReject() {
+  filteredSection.innerHTML = '';
+  for (let rejecte of rejectedList) {
     console.log(interview);
     let div = document.createElement('div');
     div.innerHTML = `<div
@@ -122,5 +231,8 @@ function toggleStyle(id) {
   } else if (id == 'all-filter-btn') {
     allCards.classList.remove('hidden');
     filteredSection.classList.add('hidden');
+  } else if (id == 'rejected-filter-btn') {
+    allCards.classList.add('hidden');
+    filteredSection.classList.remove('hidden');
   }
 }
